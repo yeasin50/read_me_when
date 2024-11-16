688da0a914890936f0bf000b5b7d5172abaae3e8
Md. Yeasin Sheikh <yeasinsheikh50@gmail.com>
2024-11-16 21:14:31 +0600

    updated

--- lib/src/insfrastructure/enum/mood.dart
+++ lib/src/insfrastructure/enum/mood.dart
@@ -1,3 +1,7 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
 enum Mood {
   sad,
   happy,
@@ -9,4 +13,8 @@ enum Mood {
   love,
   surprise,
   disgust,
+}
+
+extension MoodColor on Mood {
+  Color get color => Colors.cyan;
 }
